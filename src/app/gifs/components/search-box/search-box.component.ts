2c6d070dd6c4905ced906f9ab6c45b7b88c7c9d9
Jonathan <contactoanonimo.x@gmail.com>
2024-09-23 21:37:41 -0600

    viewChild

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
@@ -8,5 +8,17 @@ import { Component } from '@angular/core';
 export class SearchBoxComponent {
   constructor() {
 
+  }
+
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+  // searchTag(newTag: string) {
+  //   console.log(newTag)
+  // }
+
+  searchTag() {
+    const newTag = this.tagInput.nativeElement.value;
+    console.log(newTag)
   }
 }
