bbdc22726d4c0532d4eda2e755824bd35d8581b2
Jonathan <contactoanonimo.x@gmail.com>
2024-09-23 22:49:15 -0600

    apikey

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -5,12 +5,30 @@ export class GifsService {
   constructor() { }
 
   private _tagsHistory: string[] = []
+  private apiKey: string = 'et1SAa16WV7v07PWr1zyW2HeRDY1ekSg'
 
   get tagHistory() {
     return [...this._tagsHistory];
   }
 
   public searchTag(tag: string): void {
-    this._tagsHistory.unshift(tag);
+
+    if (tag.length === 0)
+      return;
+
+    this.organizeHistory(tag);
+
+  }
+
+  private organizeHistory(tag: string) {
+    tag = tag.toLowerCase();
+
+    if (this._tagsHistory.includes(tag)) {
+      this._tagsHistory = this._tagsHistory.filter(ele => ele !== tag)
+    }
+
+    this._tagsHistory.unshift(tag)
+    this._tagsHistory = this._tagsHistory.splice(0, 10)
+
   }
 }
