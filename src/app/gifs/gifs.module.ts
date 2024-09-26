178830ac3b787d959f1512aeba1e237fa4d1d5cd
Jonathan <contactoanonimo.x@gmail.com>
2024-09-26 11:03:00 -0600

    Fin de App

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+
 import { HomePageComponent } from './pages/home/home-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
 import { CardListComponent } from './components/card-list/card-list.component';
 import { GifsCardComponent } from './components/card/card.component';
-
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { GifsCardComponent } from './components/card/card.component';
     GifsCardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [
     HomePageComponent
